4b65e68c57b059f25905958b2a225cdf122b75b1
lunu <fairus200599@gmail.com>
2022-11-25 17:11:18 +0300

    Part 8 lesson

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,5 +3,7 @@ class Question < ApplicationRecord
   belongs_to :best_answer, class_name: 'Answer', foreign_key: :answer_id, optional: true, inverse_of: :question
   belongs_to :user
 
+  has_many_attached :files
+
   validates :title, :body, presence: true
 end
